a48ff04c8d01be7fef10ac707c55e16b477a43b3
Munna-Scripts <munnascriptz@gmail.com>
2025-11-19 17:02:13 +0600

    Navbar and banner aos added

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,21 +4,21 @@ import { Link } from 'react-router'
 const Banner = () => {
   return (
     <>
-        <section id='Banner' className='mt-[40px]'>
+        <section id='Banner' className='mt-[40px] overflow-hidden'>
             <div className="container">
                 <div id="Banner-Row" className='flex lg:flex-row flex-col items-center justify-between'>
                     {/* ---------------Left Side---------------- */}
                     <div className='lg:w-[680px] w-full flex lg:items-center justify-center flex-col'>
                         <div>
-                            <h1 className='lg:text-center text-primary lg:text-[48px] text-[32px] font-semibold font-siliguri lg:leading-[57px]'>আমরা প্রস্তুত আপনার ব্যবসায়িক সফলতা নিশ্চিত করতে</h1>
-                            <p className='text-brand lg:text-xl text-base lg:text-center mt-5 mb-13 font-siliguri font-medium'>আমাদের লক্ষ্য হলো আপনার ব্যবসাকে ডিজিটাল দুনিয়ায় শীর্ষে পৌঁছে দেওয়া এবং আপনার ব্র্যান্ডকে আরও শক্তিশালী করা</p>
+                            <h1 className='lg:text-center text-primary lg:text-[48px] text-[32px] font-semibold font-siliguri lg:leading-[57px]' data-aos="fade-right" data-aos-easing="ease-in-sine" data-aos-delay="1200">আমরা প্রস্তুত আপনার ব্যবসায়িক সফলতা নিশ্চিত করতে</h1>
+                            <p className='text-brand lg:text-xl text-base lg:text-center mt-5 mb-13 font-siliguri font-medium' data-aos="fade-right" data-aos-easing="ease-in-sine" data-aos-delay="1400">আমাদের লক্ষ্য হলো আপনার ব্যবসাকে ডিজিটাল দুনিয়ায় শীর্ষে পৌঁছে দেওয়া এবং আপনার ব্র্যান্ডকে আরও শক্তিশালী করা</p>
                         </div>
-                        <Link to={'/'} className='lg:py-[18px] w-fit py-[14px] px-[45px] rounded-full font-siliguri bg-primary text-white cursor-pointer text-lg'>
+                        <Link to={'/'} className='lg:py-[18px] w-fit py-[14px] px-[45px] rounded-full font-siliguri bg-primary text-white cursor-pointer text-lg' data-aos="fade-right" data-aos-easing="ease-in-sine" data-aos-delay="1800">
                             যোগাযোগ করুন
                         </Link>
                     </div>
                     {/* ---------------Right Side---------------- */}
-                    <div>
+                    <div data-aos="fade-left" data-aos-easing="ease-in-sine" data-aos-delay="1600">
                         <img src={bannerImg} alt="Banner Image" />
                     </div>
                 </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,19 +4,19 @@ import { Link } from 'react-router'
 const Navbar = () => {
   return (
     <>
-        <nav className='py-[20px] lg:block hidden'>
+        <nav className='py-[20px] lg:block hidden' data-aos="fade-down">
             <div className="container">
                 <div id="Nav-Row" className='flex items-center justify-between'>
                     {/* -----------------Logo-------------------- */}
-                    <Link to={'/'}><img className='w-[80px]' src={logo} alt="Logo" /></Link>
+                    <Link to={'/'}><img className='w-[80px]' src={logo} alt="Logo" data-aos="fade-down" data-aos-easing="ease-in-sine" data-aos-delay="800"/></Link>
                     {/* -----------------Nav links-------------------- */}
                     <div className='flex items-center gap-8 text-xl font-poppins'>
-                        <Link className='hover:text-primary duration-300' to={'/'}>Home</Link>
-                        <Link className='hover:text-primary duration-300' to={'/'}>About Us</Link>
-                        <Link className='hover:text-primary duration-300' to={'/'}>Services</Link>
+                        <Link className='hover:text-primary duration-300' to={'/'} data-aos="fade-down" data-aos-easing="ease-in-sine" data-aos-delay="1000">Home</Link>
+                        <Link className='hover:text-primary duration-300' to={'/'} data-aos="fade-down" data-aos-easing="ease-in-sine" data-aos-delay="1200">About Us</Link>
+                        <Link className='hover:text-primary duration-300' to={'/'} data-aos="fade-down" data-aos-easing="ease-in-sine" data-aos-delay="1400">Services</Link>
                     </div>
                     {/* -----------------Nav Buttons-------------------- */}
-                    <Link to={'/'} className='py-[18px] px-[45px] font-siliguri rounded-full bg-primary text-white cursor-pointer text-lg'>
+                    <Link to={'/'} className='py-[18px] px-[45px] font-siliguri rounded-full bg-primary text-white cursor-pointer text-lg' data-aos="fade-down" data-aos-easing="ease-in-sine" data-aos-delay="1800">
                         যোগাযোগ করুন
                     </Link>
                 </div>
